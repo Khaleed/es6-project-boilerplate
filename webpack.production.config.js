4a5dbcfdd9a0aba47084565cb251df4667e626ad
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-18 01:22:29 +0100

    add a webpack production config file

---
+++ webpack.production.config.js
@@ -0,0 +1,63 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    // app entry point
+    entry: path.resolve(__dirname, 'app', 'index.js'),
+
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    // define module loaders
+    module: {
+        loaders: [
+            // ES6 loader
+            {
+                test: /\.jsx?$/, // match loader to file
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel?optional[]=runtime'
+            },
+            // JSON loader
+            {
+                test: /\.json$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'json'
+            },
+            // CSS Loader
+            {
+                test: /\.css$/,
+                // load css file into it's own file
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+        ]
+    },
+    // resolution
+    resolve: {
+        extensions: ['.js', '.json', '']
+    },
+
+    plugins: [
+        // Extract CSS files
+        new ExtractTextPlugin("style.css"),
+        // Avoid duplicated stuff
+        new webpack.optimize.DedupePlugin(),
+        // Optimise occurence order
+        new webpack.optimize.OccurenceOrderPlugin(),
+        // only for production
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        // minimise output chunks of scripts/css
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ]
+};
