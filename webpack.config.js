486b9420ba5ac7ef64a0c4a81c38461d76d0c75a
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-18 01:19:45 +0100

    add a webpack dev config file

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    // console errors map to correct file and line number
+    devtool: 'eval-source-map',
+    // app entry point
+    entry: [
+        // refresh browser on none HMR updates
+        'webpack-dev-server/client?http://localhost:3000',
+        // save code -> gets injected into page without refresh
+        'webpack/hot/dev-server',
+        // current app
+        path.resolve(__dirname, 'app', 'index.js')
+    ],
+    // where to dump bundled file
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    // define module loaders
+    module: {
+        loaders: [
+            // ES6 loader
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel?optional[]=runtime'
+            },
+            // JSON loader
+            {
+                test: /\.json$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'json'
+            },
+            // CSS Loader
+            {
+                test: /\.css$/,
+                // load css files
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+        ]
+    },
+    // resolution
+    resolve: {
+        extensions: ['.js', '.json', '']
+    },
+
+    plugins: [
+        // Extract CSS files
+        new ExtractTextPlugin("style.css"),
+        // Hot module replacement
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
